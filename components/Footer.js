037eb5d6035c35d1cb09b9ba151dbea612e45802
Prathik Reddy <tvkp333555@gmail.com>
2022-04-02 23:32:50 +0530

    designing changes

--- components/Footer.js
+++ components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
     return (
         <div>
             <footer className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+                <div className="container px-5 pb-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
                     <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
                         <Link href={"/"}>
                             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                                <Image src="/logo.png" alt="" width="200" height="80"/>
+                                <Image src="/logo1.jpg" alt="" width="200" height="80"/>
                             </a>
                         </Link>
 
