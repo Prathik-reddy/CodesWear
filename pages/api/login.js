b0dfa0a718085d1fe60021284b6c1ef11871c889
Prathik Reddy <tvkp333555@gmail.com>
2022-04-13 20:38:24 +0530

    added encryption to login and signup

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,20 @@
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose"
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
     if (req.method == "POST") {
-        let user = await User.findOne({"email":req.body.email})
-        if(user){
-            if(req.body.email == user.email && req.body.pass == user.pass){
-                res.status(200).json({ success:true,email:user.email,name:user.name})
-            }else{
-                res.status(400).json({ success:false,msg:"invalid credentials"});
+        let user = await User.findOne({ "email": req.body.email })
+        let bytes = CryptoJS.AES.decrypt(user.pass, 'secret123');
+        let decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+        if (user) {
+            if (req.body.email == user.email && req.body.pass == decryptedData) {
+                res.status(200).json({ success: true, email: user.email, name: user.name })
+            } else {
+                res.status(400).json({ success: false, msg: "invalid credentials" });
             }
-        }else{
-            res.status(400).json({ success:false,msg:"no such user found"});
+        } else {
+            res.status(400).json({ success: false, msg: "no such user found" });
         }
     }
     else {
--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -1,9 +1,11 @@
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose"
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
     if (req.method == "POST") {
-        let u = new User(req.body)
+        const {name,email} = req.body
+        let u = new User({name, email,pass:CryptoJS.AES.encrypt(req.body.pass,"secret123").toString()})
         await u.save();
         res.status(200).json(u)
     }
