843f106c5e7f54d29269b97f6c3cc9efa1e76ac8
Prathik Reddy <tvkp333555@gmail.com>
2022-04-14 11:57:00 +0530

    desing of orders page

--- pages/orders.js
+++ pages/orders.js
@@ -1,46 +1,87 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import mongoose from 'mongoose';
+import Order from '../models/Order';
 
-const orders = () => {
-
+const Orders = () => {
     return (
-        <section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-                        <h2 className="text-sm title-font text-gray-500 tracking-widest">CodesWear.com</h2>
-                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">OrderId : #13535</h1>
-                        <p className="leading-relaxed mb-4">Your order has been successfully placed.</p>
-                        <div className="flex mb-2">
-                            <a className="flex-grow text-center text-orange-500 py-2 text-lg px-1">Description</a>
-                            <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Quantity</a>
-                            <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Total</a>
-                        </div>
-                        <div className="flex border-t border-gray-200 py-2">
-                            <span className="text-gray-500">Wear the code : xl-black</span>
-                            <span className="mx-auto text-gray-900">1</span>
-                            <span className="mx-auto text-gray-900">₹499</span>
-                        </div>
-                        <div className="flex border-t border-gray-200 py-2">
-                        <span className="text-gray-500">Wear the code : xl-black</span>
-                            <span className="mx-auto text-gray-900">1</span>
-                            <span className="mx-auto text-gray-900">₹499</span>
-                        </div>
-                        <div className="flex border-t border-b mb-6 border-gray-200 py-2">
-                        <span className="text-gray-500">Wear the code : xl-black</span>
-                            <span className="mx-auto text-gray-900">1</span>
-                            <span className="mx-auto text-gray-900">₹499</span>
-                        </div>
-                        <div className="flex">
-                            <span className="title-font font-medium text-2xl text-gray-900">₹1500.00</span>
-                            <button className="flex ml-auto text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded">Track Order</button>
+        <div className="container  mx-auto">
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <h1 className="font-semibold text-center p-5 text-2xl">My Orders</h1>
+                            <table className="min-w-full">
+                                <thead className="bg-white border-b">
+                                    <tr>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            #
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            First
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Last
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Handle
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Mark
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Otto
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            @mdo
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Jacob
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Thornton
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            @fat
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Larry
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Wild
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            @twitter
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
+export async function getServerSideProps(context) {
 
-export default orders
+    if (!mongoose.connections[0].readyState) {
+        await mongoose.connect(process.env.MONGO_URI)
+    }
+    let orders = await Order.find({})
+
+    return {
+        props: { orders: orders },
+    }
+}
+export default Orders
